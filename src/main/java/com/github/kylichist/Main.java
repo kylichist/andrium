92f4a5dd927191f6f814ee0cb6f5fdadcf2044a0
kylichist <pimenovpetya@gmail.com>
2020-03-17 19:12:36 +0300

    Initial commit

---
+++ src/main/java/com/github/kylichist/Main.java
@@ -0,0 +1,7 @@
+package com.github.kylichist;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/github/kylichist/server/Server.java
@@ -0,0 +1,4 @@
+package com.github.kylichist.server;
+
+public class Server {
+}
